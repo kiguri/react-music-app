f82623fd8e10da6e24177ce496f6a1af4356dd3f
Kiguri <kiguri@Kiguris-iMac.local>
2020-11-18 10:24:31 +0700

    added data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+
+//import styles
 import './styles/app.scss'
+
+//import components
+import Player from './components/Player'
+import Song from './components/Song'
+
+//import data
+import data from './data'
 
 function App() {
     return (
         <div>
-            <h1>Music Player</h1>
+            <Song />
+            <Player />
         </div>
     )
 }
