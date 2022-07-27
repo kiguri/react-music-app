c623cc5754b702f9113f8fe5058632bc474a00f7
HuyHa <huyha14082012@gmail.com>
2022-07-27 16:20:01 +0700

    animate

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
-import { useState, useRef } from 'react'
+import { useState, useRef } from "react";
 //import styles
-import './styles/app.scss'
+import "./styles/app.scss";
 
 //import components
-import Player from './components/Player'
-import Song from './components/Song'
-import Library from './components/Library'
-import Nav from './components/Nav'
+import Player from "./components/Player";
+import Song from "./components/Song";
+import Library from "./components/Library";
+import Nav from "./components/Nav";
 
 //import data
-import { data } from './data'
+import { data } from "./data";
 
 function App() {
-    //Ref
-    const audioRef = useRef(null)
-    //States
-    const [songs, setSongs] = useState(data)
-    const [currentSong, setCurrentSong] = useState(songs[0])
-    const [isPlaying, setIsPlaying] = useState(false)
-    const [libraryStatus, setLibraryStatus] = useState(false)
+  //Ref
+  const audioRef = useRef(null);
+  //States
+  const [songs, setSongs] = useState(data);
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [libraryStatus, setLibraryStatus] = useState(false);
 
-    return (
-        <div className={`App ${libraryStatus ? 'library-active' : ''}`}>
-            <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus}/>
-            <Song  currentSong={currentSong} />
-            <Player 
-                audioRef={audioRef}
-                isPlaying={isPlaying} 
-                setIsPlaying={setIsPlaying}
-                songs={songs}
-                setSongs={setSongs}
-                currentSong={currentSong}
-                setCurrentSong={setCurrentSong}
-            />
-            <Library 
-                songs={songs}
-                currentSong={currentSong}
-                setCurrentSong={setCurrentSong}
-                libraryStatus={libraryStatus}
-            />
-        </div>
-    )
+  return (
+    <div className={`App ${libraryStatus ? "library-active" : ""}`}>
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+      <Song isPlaying={isPlaying} currentSong={currentSong} />
+      <Player
+        audioRef={audioRef}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        songs={songs}
+        setSongs={setSongs}
+        currentSong={currentSong}
+        setCurrentSong={setCurrentSong}
+      />
+      <Library
+        songs={songs}
+        currentSong={currentSong}
+        setCurrentSong={setCurrentSong}
+        libraryStatus={libraryStatus}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Song.js
+++ src/components/Song.js
@@ -1,11 +1,11 @@
-const Song = ({ currentSong: { cover, name, artist} }) => {
-    return ( 
-        <div className="song-container">
-            <img src={cover} alt={name} />
-            <h2>{name}</h2>
-            <h3>{artist}</h3>
-        </div>
-     );
-}
- 
+const Song = ({ isPlaying, currentSong: { cover, name, artist } }) => {
+  return (
+    <div className="song-container">
+      <img src={cover} alt={name} className={!isPlaying && "pause"} />
+      <h2>{name}</h2>
+      <h3>{artist}</h3>
+    </div>
+  );
+};
+
 export default Song;
